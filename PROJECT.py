132ed624f35d1873a41c7cc471b962ffd501a9db
zarafarooqui <engrimsha@gmail.com>
2024-12-21 18:46:14 +0500

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,14 @@
+#Swapping operation
+#Take input values from user
+x=input("Enter Value of x:")
+y=input("Enter Value of y:")
+z=input("Enter Value of z:")
+#Swapping
+temp=x
+x=y 
+y=z
+z=temp
+#Displayiong results after swapping
+print("values of x after swapping",x)
+print("values of y after swapping",y)
+print("values of z after swapping",z)
